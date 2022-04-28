85680cf09366e3333a701a1ec8d9c21bc385a9ab
bkn.ngeno@gmail.com <bkn.ngeno@gmail.com>
2022-04-28 20:15:36 +0300

    updated url for single blog with pagination applied

--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,17 @@ import requests
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+
+def blog(request):
+    blog = Blog.objects.all()
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(blog, 9)
+    try:
+        blogs = paginator.page(page)
+    except PageNotAnInteger:
+        blogs = paginator.page(1)
+    except EmptyPage:
+        blogs = paginator.page(paginator.num_pages)
+    return render(request, 'main/blog.html', locals())
