017b163d2d4a1a8f5103622a4892eda04cdd8c04
bkn.ngeno@gmail.com <bkn.ngeno@gmail.com>
2022-04-28 20:13:57 +0300

    added url path to single blog

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='landing'), 
+    path('blog/',views.blog,name='blog'),
+    path('blog/<slug:slug>_<int:pk>',views.singleblog,name='singleblog'),
 ]
