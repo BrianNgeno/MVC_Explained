3269e0304f0b20284026e4bb6f2cb572c52a9173
bkn.ngeno@gmail.com <bkn.ngeno@gmail.com>
2022-04-28 20:35:18 +0300

    updated static configurations on settings and updated on the path file

--- main/settings.py
+++ main/settings.py
@@ -140,4 +140,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+DISABLE_COLLECTSTATIC=1
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
