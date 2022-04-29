efecf15c76f99ee60145426a75a93df98bd042f3
bkn.ngeno@gmail.com <bkn.ngeno@gmail.com>
2022-04-30 01:07:10 +0300

    installed cloudinary and added to app settings

--- main/settings.py
+++ main/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
-import dj_database_url
-from decouple import config,Csv
 
+import cloudinary
+import cloudinary.api
+import cloudinary.uploader
+import dj_database_url
+import django_heroku
+from decouple import Csv, config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -70,6 +73,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'django_summernote',
+    'cloudinary'
 ]
 
 MIDDLEWARE = [
