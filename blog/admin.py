a3776441d64ab1c8bde668313132fde7b4c263e8
bkn.ngeno@gmail.com <bkn.ngeno@gmail.com>
2022-04-29 23:49:10 +0300

    registered blog app in admin page

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
-from django.contrib import admin
+from django.contrib import admin 
+from .models import Blog
+from django_summernote.admin import SummernoteModelAdmin
+
 
 # Register your models here.
+
+class BlogAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
+
+admin.site.register(Blog,BlogAdmin)
