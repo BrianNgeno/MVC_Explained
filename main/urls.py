e4a656db5ff035a0de426e5cff68e639c4566ba2
bkn.ngeno@gmail.com <bkn.ngeno@gmail.com>
2022-04-30 00:05:10 +0300

    updated django summernote urls to be included in the app urls

--- main/urls.py
+++ main/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls'))
+    path('', include('blog.urls')),
+    path('summernote/', include('django_summernote.urls')), # using include('django_summernote.urls')
+    path('editor/', include('django_summernote.urls')) # using include('django_summernote.urls') once again
 ]
