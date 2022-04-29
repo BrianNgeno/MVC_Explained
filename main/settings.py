6a16ea1c77c63aabeda86458622c08529064cfd8
bkn.ngeno@gmail.com <bkn.ngeno@gmail.com>
2022-04-30 01:24:21 +0300

    added clodinary apikey, secret and cloudname to the setting.py and updated on env

--- main/settings.py
+++ main/settings.py
@@ -149,3 +149,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
+cloudinary.config( 
+  cloud_name = config('CLOUD_NAME'),
+  api_key = config('API_KEY'),
+  api_secret = config('API_SECRET')
+)
