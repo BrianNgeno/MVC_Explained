fbe3df7807b6a6be490f1df9b9f427b32a51de03
bkn.ngeno@gmail.com <bkn.ngeno@gmail.com>
2022-04-30 00:01:00 +0300

    added psycopg2 to application and updated on requirements

--- main/settings.py
+++ main/settings.py
@@ -126,17 +126,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
 USE_TZ = True
-
+TIME_ZONE = 'Africa/Nairobi'
+USE_I18N = True
+USE_L10N = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
