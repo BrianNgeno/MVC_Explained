f35dc4133935efdfb994c124a2f0da53613ed046
bkn.ngeno@gmail.com <bkn.ngeno@gmail.com>
2022-04-30 02:27:11 +0300

    added a Procfile to specify the commands that are executed by the app’s dynos

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
-
+from models import Blog
 # Create your tests here.
+class BlogTestClass(TestCase):
+    """
+    Test blog class and its functions
+    """
+    def setUp(self):
+        #creating a new instance
+        self.blog = Blog( time_created='2006-10-25',publisher='Brian Ngeno',title='informative', content='This is to explain on the MVC model', slug="mvc_framework_explained",image='blog_image.png')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.blog, Blog))
